127245b8d8443ac41646fa1c8d3d0a0ece94682a
Anthony Fammartino <afammart@gmail.com>
2017-07-18 23:10:05 -0700

    various changes

--- image_handler.py
+++ image_handler.py
@@ -5,13 +5,18 @@ from PIL import Image
 
 
 def get_text_from_image(image_file):
-    return pytesseract.image_to_string(Image.open(image_file))
+    image = Image.open(image_file)
+    return pytesseract.image_to_string(image)
 
 def extract_frames(in_gif='current_recipe.gif', out_folder='images'):
     frame = Image.open(in_gif)
     nframes = 0
     while frame:
-        frame.save( '%s/%s-%06d.png' % (out_folder, os.path.basename(in_gif), int(nframes)))
+        save_file = os.path.join(
+            out_folder,
+            "{}-{:06d}.png".format(os.path.basename(in_gif), int(nframes))
+        )
+        frame.save(save_file)
         nframes += 1
         try:
             frame.seek( nframes )
@@ -19,9 +24,9 @@ def extract_frames(in_gif='current_recipe.gif', out_folder='images'):
             break;
     return True
 
-def download_gif(uri):
+def download_gif(uri, out_file='current_recipe.gif'):
     response = requests.get(uri)
     if response.status_code == 200:
-        with open('current_recipe.gif', 'wb') as f:
+        with open(out_file, 'wb') as f:
             for chunk in response.iter_content(4096):
                 f.write(chunk)
--- reddit_handler.py
+++ reddit_handler.py
@@ -3,6 +3,7 @@ import praw
 import time
 from image_handler import download_gif, extract_frames, get_text_from_image
 
+
 reddit = praw.Reddit('gif_recipes_bot')
 
 # Good test urls
@@ -10,15 +11,22 @@ reddit = praw.Reddit('gif_recipes_bot')
 
 def main(reddit=reddit):
     gif_recipes_subreddit = reddit.subreddit('gifrecipes')
-    for submission in gif_recipes_subreddit.hot(limit=10):
-        if submission.url[-4:] == 'gifv':
-            download_gif(submission.url[0:-1])  # download the .gif not .gifv
-            extract_frames()
-            for f in os.listdir('images'):
-                print('-' * 20, f, '-' * 20)
-                if f != '.placeholder':
-                    print(get_text_from_image('images/{}'.format(f)))
-            return  # return after one for now
+    try:
+        for submission in gif_recipes_subreddit.top(limit=10):
+            if submission.url[-4:] == 'gifv':
+                download_gif(submission.url[0:-1])  # download the .gif not .gifv
+                extract_frames()
+                for f in os.listdir('images'):
+                    if f != '.placeholder':
+                        print('-' * 20, f, '-' * 20)
+                        print(get_text_from_image(os.path.join('images', f)))
+                break  # return after one for now
+    finally:
+        try:
+            os.remove('current_recipe.gif')
+        except FileNotFoundError:
+            pass
+        [os.remove(os.path.join('images', f)) for f in os.listdir('images') if f.endswith('.png')]
 
 if __name__ == '__main__':
     main()
